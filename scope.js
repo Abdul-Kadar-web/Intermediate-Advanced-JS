f773a235b9ca6e3e0642a77d7ba0be606d940c07
agforhad <abdulgaffer1100@gmail.com>
2021-02-02 15:46:33 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,33 @@
+// jekhane likba seta jodi let ba const dia hoi seta scope er bahire gia hoi na but var dia korle scope bahir a geleo hoi.
+
+// function sum(first, second){
+//     let result = first + second;
+//     console.log(result);
+//     return result;
+// }
+
+// const output = sum(3, 7);
+// console.log(output);
+
+
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(result);
+    if(result > 9){
+        var mood = "happy"
+        console.log(mood);
+    }
+    // var console.log er niche lhikle undefined dekhabe but let const hoi kicui asbe na 
+    console.log(day);
+    var day = "friday";
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+
+
+// ThatJsDude----article---Google it
