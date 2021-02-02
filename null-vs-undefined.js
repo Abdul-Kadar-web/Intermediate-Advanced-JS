6e9bb51a728545fc64638ed88d54dc650688dd48
agforhad <abdulgaffer1100@gmail.com>
2021-02-02 14:42:35 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,34 @@
+// undefined
+
+// pakhir value na dewa
+// let pakhi;
+// console.log(pakhi);
+
+// return na korai
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(13, 82);
+// console.log(result);
+
+// jeta k call korsi oita na dewa jodi na fete chai default value dile hoi
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+// console.log(result);
+
+// property ta object er moddeh set kora nai
+// const premik = {name:"smart dude", phone:123456789};
+// console.log(premik.gf);
+
+// value declar kore undefined dile value undefined a asbe
+// let fun = undefined;
+// console.log(fun);
+
+
+let ages = [2, 5, 9]
+console.log(ages[11]);
+// null
+
