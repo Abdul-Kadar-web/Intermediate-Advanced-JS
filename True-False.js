fe7eca89ed8cc767e6914054063eb6d633cf7cbe
agforhad <abdulgaffer1100@gmail.com>
2021-02-02 14:24:06 +0600

    true-false

---
+++ True-False.js
@@ -0,0 +1,19 @@
+ // is true other than 0
+ // is true other than empty string("")  NaN is also falsy
+// undefined
+//null
+// written false
+
+
+// Truthy
+// '0', ' ' []
+// const age = 4
+// const name = "solaiman"
+let name = 0
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
